0394b43067d3119709d3a4ca791c9072b9f7348a
Cybersport <Cybersport@26kadr.internal>
2024-10-08 11:21:11 +0300

    first commit

---
+++ app/src/main/java/com/example/arsen/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.arsen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void NavigateTo(View view) {
+    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+    startActivity(intent);
+    NavigateToff();
+    }
+    public void NavigateToff() {
+        Toast.makeText(MainActivity.this,"О прилодении",Toast.LENGTH_SHORT).show();
+    }
+
+    public void NavigateToss(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+        NavigateTosf();
+    }
+    public void NavigateTosf() {
+        Toast.makeText(MainActivity.this,"информация",Toast.LENGTH_SHORT).show();
+    }
+}
